99578889777526c90c4efc189c27474d61f3811d
Sam Gwydir <gwydir8@gmail.com>
2012-07-21 19:16:07 -0500

    first commit

---
+++ custom.el
@@ -0,0 +1,60 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(background-color "#002b36")
+ '(background-mode dark)
+ '(calendar-week-start-day 1)
+ '(column-number-mode t)
+ '(cursor-color "#839496")
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-safe-themes (quote ("5726fd11f2abcc30fa22eade5afe6d74a1ebbc187a9b606eca497094654984a0" "1d1e97c4804a04441b08226e4775295843baae033673e63513ae925eafcdc9bf" "8b49009d04730bc5d904e7bb5c7ff733f3f9615c3d6b3446eca0766e6da2bea1" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "117069e595a587a5bec38c3e5fffad95f303b9a6d0ed83aae4eae47362bc8515" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "b45ad13a77f99d4d734456579ee79e88a6fdc3a3bec0dbe769ddfbbaf57d6c40" "baed08a10ff9393ce578c3ea3e8fd4f8c86e595463a882c55f3bd617df7e5a45" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "d589f8adcca47e586469f7719e11a1d3ead95d13bf365ac0ae15b04fa6ca7c93" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(default-frame-alist (quote ((vertical-scroll-bars))))
+ '(delete-by-moving-to-trash t)
+ '(desktop-save-mode nil)
+ '(diary-show-holidays-flag nil)
+ '(doc-view-continuous t)
+ '(evil-esc-delay 0.0)
+ '(fci-rule-color "#383838")
+ '(foreground-color "#839496")
+ '(fringe-mode 0 nil (fringe))
+ '(indicate-empty-lines nil)
+ '(iswitchb-mode t)
+ '(linum-format " %3d ")
+ '(markdown-command "multimarkdown")
+ '(midnight-mode t nil (midnight))
+ '(ns-pop-up-frames nil)
+ '(org-agenda-files (quote ("~/Notes/isms.org" "~/Notes/buy.org" "~/Notes/general.org" "~/Notes/habits.org" "~/Notes/money.org" "~/Notes/school.org" "~/Notes/houston.org" "~/Notes/collegestation.org")))
+ '(org-agenda-follow-indirect t)
+ '(org-agenda-include-diary t)
+ '(org-agenda-skip-deadline-prewarning-if-scheduled nil)
+ '(org-agenda-span (quote day))
+ '(org-agenda-time-grid (quote ((daily today require-timed remove-match) #("----------------" 0 16 (org-heading t)) (500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2100 2200))))
+ '(org-agenda-time-leading-zero t)
+ '(org-columns-default-format "%25ITEM %TODO %3PRIORITY %TAGS %17Effort(Estimated Effort){:} %CLOCKSUM")
+ '(org-enforce-todo-checkbox-dependencies t)
+ '(org-enforce-todo-dependencies t)
+ '(org-global-properties (quote (("Effort_ALL" . "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))))
+ '(org-hide-leading-stars t)
+ '(org-indent-mode-turns-on-hiding-stars t)
+ '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m)))
+ '(org-scheduled-past-days 2)
+ '(prelude-use-default-prelude-theme nil)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(solarized-broken-srgb nil)
+ '(solarized-contrast (quote normal))
+ '(trash-directory "~/.Trash/")
+ '(truncate-lines t))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 200 :family "Consolas"))))
+ '(org-hide ((t (:foreground "background")))))
---
+++ sam.el
@@ -0,0 +1,284 @@
+;;emacs daemon
+(server-start)
+
+;;path from zshrc
+(if (not (getenv "TERM_PROGRAM"))
+    (setenv "PATH"
+            (shell-command-to-string "source $HOME/.zshrc && printf $PATH")))
+
+;;load personal/vendor and it's subdirs
+(let ((default-directory "~/.emacs.d/personal/vendor/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;;load themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized-emacs")
+
+;;;requires
+(require 'org-install)
+
+(require 'org-crypt)
+(require 'epa-file)
+
+(require 'dired-x)
+
+(require 'paredit)
+
+(require 'undo-tree)
+(require 'evil)
+(require 'evil-leader)
+(require 'evil-numbers)
+(require 'surround)
+
+;;prelude-modules
+(require 'prelude-programming)
+(require 'prelude-c)
+(require 'prelude-css)
+(require 'prelude-clojure)
+(require 'prelude-coffee)
+(require 'prelude-common-lisp)
+(require 'prelude-emacs-lisp)
+(require 'prelude-haskell)
+;(require 'prelude-erlang)
+(require 'prelude-js)
+;(require 'prelude-latex)
+(require 'prelude-org)
+(require 'prelude-perl)
+(require 'prelude-python)
+(require 'prelude-ruby)
+(require 'prelude-scheme)
+(require 'prelude-scss)
+(require 'prelude-xml)
+;; tools & utilities support
+(require 'prelude-erc)
+(require 'prelude-mediawiki)
+
+;;midnight mode
+(setq midnight-period 7200)
+
+;;focus follows mouse
+(setq mouse-autoselect-window t)
+
+;;encryption
+(setq epg-gpg-program "gpg2")
+(setenv "GPG_AGENT_INFO" "~/.gnupg/gpg-agent.env")
+;(setenv "GPG_AGENT_INFO" nil)
+;; use keys
+(setq epa-file-select-keys nil)
+(setq epa-file-inhibit-auto-save t)
+
+;;undo-tree
+(global-undo-tree-mode)
+
+;;auctex
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
+(require 'tex-site)
+
+(setq TeX-PDF-mode nil)
+(setq preview-image-type (quote dvipng))
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+
+(setq-default TeX-master nil)
+
+;;my org customizations
+;;Don't like the way this works
+;;(setq org-startup-indented t)
+
+;; Enable encryption
+(setq org-mobile-use-encryption t)
+;; Set a password
+(setq org-mobile-encryption-password "RedBook4096")
+
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance (quote ("crypt")))
+(setq org-crypt-key "0AD14C01")
+
+;org-agenda opens in current window
+(setq org-agenda-window-setup 'current-window)
+
+;;journal
+(defvar org-journal-file "~/Notes/Personal/log.org.gpg"
+  "Path to OrgMode journal file.")
+(defvar org-journal-date-format "%Y-%m-%d"
+  "Date format string for journal headings.")
+
+(defun org-journal-entry ()
+  "Create a new diary entry for today or append to an existing one."
+  (interactive)
+  (switch-to-buffer (find-file org-journal-file))
+  (widen)
+  (let ((today (format-time-string org-journal-date-format)))
+    (beginning-of-buffer)
+    (goto-line 3)
+    (unless (org-goto-local-search-headings today nil t)
+      ((lambda ()
+         (org-insert-heading)
+         (insert today)
+    (beginning-of-buffer)
+    (org-show-entry)
+    (org-narrow-to-subtree)
+    (end-of-buffer)
+    (backward-char 2)
+    (unless (= (current-column) 2)
+      (insert "\n\n  ")))))))
+
+;;capture
+(setq org-default-notes-file "~/Notes/inbox.org")
+(define-key global-map "\C-cc" 'org-capture)
+
+;;show place in file that I am in agenda...like that makes sense
+(setq org-agenda-start-with-follow-mode nil)
+
+;; start agenda from the current day.
+(setq org-agenda-start-on-weekday 0)
+
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/Notes/")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/Notes/inbox.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+
+(setq org-agenda-files (list "~/Notes/inbox.org"
+                             "~/Notes/buy.org"
+                             "~/Notes/general.org"
+                             "~/Notes/habits.org"
+                             "~/Notes/money.org"
+                             "~/Notes/school.org"
+                             "~/Notes/houston.org"
+                             "~/Notes/collegestation.org"))
+;; No properties drawer
+(setq org-mobile-force-id-on-agenda-items nil)
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d!/!)")
+              (sequence "WAITING(w@/@)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE"))))
+
+;;time tracking
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+;;time logging
+(setq org-log-done 'time)
+
+;;Evil
+(evil-mode 1)
+(setq evil-emacs-state-cursor '("#4271ae" box))
+
+;; org agenda -- leave in emacs mode but add j & k
+;(define-key org-agenda-mode-map "j" 'evil-next-line)
+;(define-key org-agenda-mode-map "k" 'evil-previous-line)
+
+(evil-set-initial-state org-agenda-mode-map 'motion)
+(evil-set-initial-state dired-mode-map 'motion)
+(global-surround-mode 1)
+;;(global-define-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
+;;(global-define-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
+
+;;Prelude-mode
+(prelude-global-mode)
+
+;;Disable Scroll Bars
+(scroll-bar-mode -1)
+
+;;Disable fringe-mode
+(set-fringe-mode 0)
+
+;;visual line mode
+(visual-line-mode 1)
+
+;;line-spacing
+(setq line-spacing 3)
+
+;;selection modification
+;; make the current 'selection' visible
+(transient-mark-mode t)
+
+;;winner-mode
+(winner-mode 1)
+
+
+;;Prog Mode Hooks
+(defun sam-prog-mode-defaults ()
+  "My coding hooks"
+  (flyspell-prog-mode)
+  (prelude-local-comment-auto-fill)
+  (prelude-turn-off-whitespace)
+  (prelude-turn-on-abbrev)
+  (prelude-add-watchwords)
+  (linum-mode)
+  (visual-line-mode 0))
+(add-hook 'prelude-prog-mode-hook 'sam-prog-mode-defaults t)
+
+;;ruby mode uses prog-mode-defaults
+(add-hook 'prelude-ruby-mode-hook 'prelue-prog-mode-hook)
+
+(blink-cursor-mode t)
+;;(global-linum-mode t)
+;;(global-hl-line-mode t)
+
+;;edit mail from mutt in mail mode
+(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
+
+;;conf-mode for certain files
+(add-to-list 'auto-mode-alist '("\\.conf$" . conf-mode))
+(add-to-list 'auto-mode-alist '("\\rc$" . conf-mode))
+(add-hook 'conf-space-mode-hook (lambda () (sam-prog-mode-defaults)) t)
+(add-hook 'conf-colon-mode-hook (lambda () (sam-prog-mode-defaults)) t)
+(add-hook 'conf-unix-mode-hook (lambda () (sam-prog-mode-defaults)) t)
+
+;;shell-script-mode for certain files
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
+(add-hook 'sh-mode-hook (lambda () (sam-prog-mode-defaults)) t)
+
+(setq org-agenda-custom-commands
+      '(("c" "Calendar" agenda ""
+         ((org-agenda-ndays 7)                          ;; [1]
+          (org-agenda-start-on-weekday 0)               ;; [2]
+          (org-agenda-time-grid t)
+          (org-agenda-repeating-timestamp-show-all t)   ;; [3]
+          (org-agenda-entry-types '(:timestamp :sexp))))  ;; [4]
+        ;; other commands go here
+        ))
+
+;;hide hidden files in dired
+(setq dired-omit-files "^\\...+$")
+(add-hook 'dired-mode-hook (lambda () (dired-omit-mode 1)))
+
+;;C-c C-c for mail-mode exit mail
+(add-hook 'mail-mode-hook
+          (lambda ()
+            (define-key mail-mode-map [(control c) (control c)]
+              (lambda ()
+                (interactive)
+                (save-buffer)
+                (server-edit)))))
+
+;;transparency in fullscreen
+
+(defvar fullscreen-parameter t)
+
+(defun toggle-fullscreen ()
+  (interactive)
+  (ns-toggle-fullscreen)
+  (setq fullscreen-parameter (not fullscreen-parameter))
+  (if fullscreen-parameter
+      (modify-frame-parameters nil `((alpha . 100)))
+      (modify-frame-parameters nil `((alpha . 90)))))
+
+;;no scroll bars
+(defun setup-frame-hook (new-frame)
+  "This function will be applied to all new emacs frames."
+  (select-frame new-frame)
+  (scroll-bar-mode 0))                   ; no scrollbar
+(add-hook 'after-make-frame-functions 'setup-frame-hook)
+
+; make completion buffers disappear after 3 seconds.
+(add-hook 'completion-setup-hook
+          (lambda () (run-at-time 3 nil
+                                  (lambda () (delete-windows-on "*Completions*"))
+                                  (lambda () (delete-windows-on "*Ido Completions*")))))
+
+;;; Shut up compile saves
+(setq compilation-ask-about-save nil)
+;;; Don't save *anything*
+(setq compilation-save-buffers-predicate '(lambda () nil))

dd42ea5cba5bcba95666b7b396049ad313de771c
Sam Gwydir <gwydir8@gmail.com>
2012-12-07 15:15:38 -0600

    Moved bindings to main binding file

--- sam-bindings.el
+++ sam-bindings.el
@@ -1,10 +1,14 @@
-;;; My Keyboard Bindings
+;;; sam-bindings.el --- My Keyboard Bindings
+
+;;; Commentary:
+;;
 
 ;;; Code:
 
 ;; Kill-buffer
 (global-set-key (kbd "s-k") 'kill-this-buffer)
 
+;;;These were for Emacs-Mac-Port, but messes up normal emacs
 ;;;; Paste
 ;;(global-set-key (kbd "s-v") 'clipboard-yank)
 ;;
@@ -13,3 +17,17 @@
 
 ;; Quit
 (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
+
+;; Smex
+(global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "C-x M-x") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; Evil
+(global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
+(global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
+
+(provide 'sam-bindings)
+
+;;; sam-bindings.el ends here
--- sam-evil.el
+++ sam-evil.el
@@ -1,25 +1,19 @@
-;;; Evil
-;; evil requires undo-tree
-(require 'undo-tree)
-(global-undo-tree-mode)
+;;; sam-evil.el --- Evil, the vim emulator, Config
 
-(require 'evil)
-(require 'evil-leader)
+;;; Commentary:
+;;
+
+;;; Code:
+
 (evil-mode 1)
+(global-surround-mode 1)
 
-;; Doesnt work
 (setq evil-normal-state-cursor '("#ac4142" box))
 (setq evil-emacs-state-cursor '("#6a9fb5" box))
 
 (evil-set-initial-state org-agenda-mode-map 'motion)
 (evil-set-initial-state dired-mode-map 'motion)
 
-(require 'surround)
-(global-surround-mode 1)
+(provide 'sam-evil)
 
-(require 'evil-numbers)
-(global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
-(global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
-
-(require 'paredit)
-(require 'evil-paredit)
+;;; sam-evil.el ends here

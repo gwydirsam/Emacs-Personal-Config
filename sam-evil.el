0723a3f7ff4fcdee9c7063eaed7a21d29334ed71
Sam Gwydir <sam@samgwydir.com>
2013-10-06 22:30:11 -0500

    Updated

--- sam-evil.el
+++ sam-evil.el
@@ -14,6 +14,9 @@
 (setq evil-normal-state-cursor '("#ac4142" box))
 (setq evil-emacs-state-cursor '("#6a9fb5" box))
 
+;; coflicts with evil
+(key-chord-mode -1)
+
 ;;(evil-set-initial-state org-agenda-mode-map 'motion)
 ;;(evil-set-initial-state dired-mode-map 'motion)
 
